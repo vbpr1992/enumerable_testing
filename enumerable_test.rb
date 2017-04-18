0da93339ed28fe31b713909b22f77c31349cf934
Valeria Benetti <vbenetti46@gmail.com>
2017-04-18 18:36:56 -0400

    9 out of 19 done and 1 still in error mode

--- enumerable_test.rb
+++ enumerable_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/nyan_cat'
 require_relative 'lib/de_enumerable'
 require_relative 'lib/book'
 
@@ -25,35 +26,49 @@ describe "Enumerable" do
   end
 
   it "implements all? correctly" do
-    skip
     results = @reimplements_enumerable.all? { |book| book.year > 1800 }
 
     assert_equal true, results
   end
 
+  it "implements all? correctly when the results are false" do
+
+    results = @reimplements_enumerable.all? { |book| book.year == 1800 }
+
+    assert_equal false, results
+  end
+
   it "implements count correctly" do
-    skip
     results = @reimplements_enumerable.count { |book| book.page_count > 300 }
 
     assert_equal 3, results
   end
 
+  it "implements count correctly" do
+    results = @reimplements_enumerable.count { |book| book.page_count == 1225 }
+
+    assert_equal 1, results
+  end
+
+  it "implements count correctly" do
+    results = @reimplements_enumerable.count { |book| book.page_count > 3000 }
+
+    assert_equal 0, results
+  end
+
   it "implements find correctly when there is a match" do
-    skip
     results = @reimplements_enumerable.find { |book| book.year > 1970 }
 
     assert_equal @h2g2, results
   end
 
   it "implements find correctly when there is not a match" do
-    skip
     results = @reimplements_enumerable.find { |book| book.year < 1492 }
 
     assert_nil results
   end
 
   it "implements each_with_index correctly" do
-    skip
     object_results = []
     index_results = []
 
@@ -67,7 +82,6 @@ describe "Enumerable" do
   end
 
   it "implements drop correctly" do
-    skip
     assert_equal [@moby_dick, @pride], @reimplements_enumerable.drop(2)
   end
 
--- your_code_here.rb
+++ your_code_here.rb
@@ -18,4 +18,54 @@ class ReimplementEnumerable
 
   # The rest of the code for all the enumerables
   # you must write go here
+  def all?
+    result = []
+
+    @collection.each do |element|
+      show_all = yield(element)
+      if show_all
+        result << element
+      end
+    end
+    result == @collection
+  end
+
+  def count
+    count = 0
+
+    @collection.each do |element|
+      show_count = yield(element)
+      if show_count
+        count += 1
+      end
+    end
+    count
+  end
+
+  def find
+
+    @collection.each do |element|
+      show_find = yield(element)
+      if show_find
+        return element
+      end
+    end
+    return nil
+  end
+
+  def each_with_index
+    index = 0
+    @collection.each do |element|
+      yield(element, index)
+      index += 1
+    end
+  end
+
+  def drop(index)
+    @collection.each do |element|
+      
+
+    end
+
+  end
 end
